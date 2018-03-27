48e83696a7c78307a5c6c8acbb16843cdc9a0984
Ethan Puerto <epuerto96@gmail.com>
2018-03-27 13:24:32 -0400

    Added Random Array Function
    Takes two imputs returns array using numpy

--- functions.py
+++ functions.py
@@ -1,3 +1,5 @@
+import numpy as np, numpy.random
+
 def percentDifference(new,old):
     """
     Parameters
@@ -9,9 +11,29 @@ def percentDifference(new,old):
 
     Returns
     -------
-    print ("lol")
     Float
         Difference bewteen new and old in percent.
-        
+
     """
     return (abs(new-old)/abs(old))*100
+
+
+
+def randomArrayist(n,p):
+    """Generates Random Array.
+
+    Parameters
+    ----------
+    n : Int
+        Length.
+    p : type
+        Width.
+
+    Returns
+    -------
+    type
+        Array
+
+    """
+    x = np.random.dirichlet(np.ones(n),size=p)
+    return x
